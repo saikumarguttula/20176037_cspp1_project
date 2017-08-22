41e3b3427e87d7376a74918342a34847b4b58141
saikumarguttula <guttullasaikumar@gmail.com>
2017-08-22 22:53:37 +0530

    added 8 text files

--- plagarism.py
+++ plagarism.py
@@ -1,20 +1,5 @@
-file = open('text1.txt', 'r')
-s1 = list(file.read().split())
-file = open('text2.txt', 'r')
-s2 = list(file.read().split())
-def lower(s1,s2):
-	global l,m
-	l,m = [],[]
-	for i in (s1):
-		k = i.lower()
-		l.append(k)
-	for i in (s2):
-		k = i.lower()
-		m.append(k)
-	return l,m
-lower(s1,s2)
-def numerator(l,m):
-	global d1,d2,dot
+def dictionary(l,m):
+	global d1,d2
 	d1,d2 = {},{}
 	for i in l:
 		if i in d1:
@@ -27,14 +12,18 @@ def numerator(l,m):
 			d2[i] = d2[i] + 1
 		else:
 			d2[i]=1
+	return d1,d2
+
+def numerator(l,m):
+	global dot
 	dot = 0
 	for i in d1:
 		for j in d2:
 			if i == j:
 				dot = dot + (d1[i]*d2[j])
 	return dot
-numerator(l,m)
-def denominator(dot):
+
+def denominator(d1,d2):
 	l1 = d1.values()
 	l2 = d2.values()
 	l1,l2 = list(l1),list(l2) 
@@ -49,6 +38,36 @@ def denominator(dot):
 	global k
 	k = answer * answer1
 	return k
-denominator(dot)
-cos = (dot/(k)) * 100
-print(str(cos) + " % matching")
+
+def answer(dot,k):
+	cos = (dot/(k)) * 100
+	return cos
+
+list0 = []
+import os
+for file in os.listdir():
+	if file.endswith(".txt"):
+		x = os.path.join(file)
+		list0.append(x)
+print(list0)
+
+import re
+sent1 = []
+for i in range(len(list0)):
+	file = open(list0[i],'r')
+	text = file.read().lower()
+	file.close()
+	text = re.sub('[^a-z\ \']+'," ",text)
+	sent = list(text.split())
+	sent1.append(sent)
+print((sent1))
+sent2 = []
+
+for i in range(len(sent1)):
+	for j in range(i,len(sent1)-1):
+		d1,d2= dictionary(sent1[i],sent1[j+1])
+		dot = numerator(d1,d2)
+		k = denominator(d1,d2)
+		cos = answer(dot,k)
+		sent2.append(cos)
+print(sent2)
